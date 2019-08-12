967e8367a808380028f9d390c4830a154362babd
Morico <moriconomori@gmail.com>
2019-08-12 16:58:13 +0900

    スタイル追加

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var app = new Vue({
     date: "yyyy/mm/dd",
     matches: 0,
     victories: 0,
-    defeats: 0
+    defeats: 0,
+    fontSize: 200
   },
   methods: {
     addWin: function (event) {
@@ -14,6 +15,24 @@ var app = new Vue({
     addLose: function (event) {
       this.matches += 1;
       this.defeats += 1;
+    },
+    minusWin: function (event) {
+      if (this.victories > 0) {
+        this.matches -= 1;
+        this.victories -= 1;
+      }
+    },
+    minusLose: function (event) {
+      if (this.defeats > 0) {
+        this.matches -= 1;
+        this.defeats -= 1;
+      }
+    },
+    fontLarge: function (event) {
+      this.fontSize += 10;
+    },
+    fontSmall: function (event) {
+      this.fontSize -= 10;
     }
   },
   mounted() {
