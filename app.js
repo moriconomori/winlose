017da10d2e0eebc2a56ec4094a1247779077f520
Morico <moriconomori@gmail.com>
2019-08-12 15:27:52 +0900

    勝敗表示

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Hello Vue!"
+    date: "yyyy/mm/dd",
+    matches: 0,
+    victories: 0,
+    defeats: 0
+  },
+  methods: {
+    addWin: function (event) {
+      this.matches += 1;
+      this.victories += 1;
+    },
+    addLose: function (event) {
+      this.matches += 1;
+      this.defeats += 1;
+    }
+  },
+  mounted() {
+    this.date = moment().format("YYYY/MM/DD");
   }
 });
